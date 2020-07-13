e7d5ba1e47cb82eb16d4e6d53a1320031c3de1fd
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-13 05:43:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+numbers = [1,2,3]
+def square_array(numbers)
+  square_array.each do |numbers|
+  puts numbers << numbers **2 
+  end
+return square_array   
 end
